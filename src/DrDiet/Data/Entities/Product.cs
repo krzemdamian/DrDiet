12d2682ea451a915ffd35f4c261690ac04d60fd3
Damian Krzeminski <krzemdamian@gmail.com>
2021-03-13 19:36:26 +0100

    Add entities

---
+++ src/DrDiet/Data/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrDiet.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        /// <summary>
+        /// Energy in kcal per kilogram
+        /// </summary>
+        public decimal Energy { get; set; }
+    }
+}
